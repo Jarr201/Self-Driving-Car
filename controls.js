ef5403d5941acfbc1edebb98396512d6745b579c
Jalen Arrington <jmarrington178@yahoo.com>
2022-11-25 03:09:24 -0500

    Added in keyboard listeners and began working on control logic

--- controls.js
+++ controls.js
@@ -5,6 +5,25 @@ class Controls{
         this.right=false;
         this.reverse=false;
 
-        
+        this.#addKeyboardListeners();
+    }
+
+    #addKeyboardListeners() {
+        document.onkeydown=(event)=>{
+            switch(event.key){
+                case "ArrowLeft":
+                    this.left=true;
+                    break;
+                case "ArrowRight":
+                    this.right=true;
+                    break;
+                case "ArrowUp":
+                    this.forward=true;
+                    break;
+                case "ArrowDown":
+                    this.reverse=true;
+                    break;
+            }
+        }
     }
 }
